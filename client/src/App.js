c54e15f198a04da7fa3fde5787b3bc22b9750adc
eduardosanzb <eduardosanzb@gmail.com>
2017-05-27 21:54:45 -0500

    Fetch Users api mock

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,33 @@
 import React, { Component } from 'react';
-import './App.css';
+import { Button } from 'office-ui-fabric-react/lib/Button';
+import './App.css'
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      users: []
+      users: null
     };
   }
 
-  componentDidMount(){
-    fetch('/users')
-      .then(res => res.json())
-      .then(users => this.setState({ users }));
+  async componentDidMount(){
+    const response = await fetch('/api');
+    const { users } = await response.json();
+    this.setState({ users })
   }
 
   render() {
-    return (
-      <div className="App">
-        <h1>Users</h1>
-        {this.state.users.map(u => <div key={u.id}>{u.username}</div>)}
-      </div>
-    );
+    if (this.state.users === null) {
+      return <div>Loading...</div>
+    } else {
+      return (
+        <div className="App">
+          <h1>Users</h1>
+          {this.state.users.map(u => <div key={u.id}>{u.username}</div>)}
+          <Button>Button</Button>
+        </div>
+      );
+    }
   }
 }
 
