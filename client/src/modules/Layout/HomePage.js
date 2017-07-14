fdea8cadc8e85999c169965ea337e00bf07470e4
eduardosanzb <eduardosanzb@gmail.com>
2017-07-13 23:11:28 -0500

    Added grid for dashboard

--- client/src/modules/Layout/HomePage.js
+++ client/src/modules/Layout/HomePage.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 import { login, fetchEvents } from '../../actions';
 import { Breadcrumb } from 'office-ui-fabric-react';
 import Layout from 'antd/lib/layout';
+import Card from 'antd/lib/card';
 // Components
 import EventsGallery from '../HomeClient/EventsGallery';
 
@@ -20,7 +21,7 @@ class Home extends React.Component {
       return (
         <div className="ms-Grid">
           <Content style={{ padding: '0 50px' }}>
-            <EventsGallery events={this.props.events}/>
+            <EventsGallery events={this.props.events} />
           </Content>
         </div>
       );
@@ -31,7 +32,70 @@ class Home extends React.Component {
       return (
         <Content style={{ padding: '0 50px' }}>
           <Breadcrumb items={[{ text: 'Home', key: 'home' }]} />
-          <h1>Hello {lastName}, {firstName}</h1>
+          <h1>
+            Hello {lastName}, {firstName}
+          </h1>
+          <div className="ms-Grid" style={{ marginTop: 10 }}>
+            <div className="ms-Grid-row">
+              <div className="ms-Grid-col ms-sm4">
+                <Card
+                  title="Card title"
+                  bordered={false}
+                  style={{ width: '100%' }}
+                >
+                  <p>Card content</p>
+                  <p>Card content</p>
+                  <p>Card content</p>
+                </Card>
+              </div>
+              <div className="ms-Grid-col ms-sm5">
+                <Card
+                  title="Card title"
+                  bordered={false}
+                  style={{ width: '100%' }}
+                >
+                  <p>Card content</p>
+                  <p>Card content</p>
+                  <p>Card content</p>
+                </Card>
+              </div>
+              <div className="ms-Grid-col ms-sm3">
+                <Card
+                  title="Card title"
+                  bordered={false}
+                  style={{ width: '100%' }}
+                >
+                  <p>Card content</p>
+                  <p>Card content</p>
+                  <p>Card content</p>
+                </Card>
+              </div>
+            </div>
+            <div className="ms-Grid-row" style={{ marginTop: 10 }}>
+              <div className="ms-Grid-col ms-sm8">
+                <Card
+                  title="Card title"
+                  bordered={false}
+                  style={{ width: '100%' }}
+                >
+                  <p>Card content</p>
+                  <p>Card content</p>
+                  <p>Card content</p>
+                </Card>
+              </div>
+              <div className="ms-Grid-col ms-sm4">
+                <Card
+                  title="Card title"
+                  bordered={false}
+                  style={{ width: '100%' }}
+                >
+                  <p>Card content</p>
+                  <p>Card content</p>
+                  <p>Card content</p>
+                </Card>
+              </div>
+            </div>
+          </div>
         </Content>
       );
     }
