9dd91ff23c134f378dfcd6073c811339a0a72713
eduardosanzb <eduardosanzb@gmail.com>
2017-07-13 22:46:05 -0500

    Added new Buildings Page

--- client/src/modules/Buildings/BuildingsPage.js
+++ client/src/modules/Buildings/BuildingsPage.js
@@ -1,45 +1,94 @@
 import React from 'react';
+import moment from 'moment';
 import { connect } from 'react-redux';
 import Layout from 'antd/lib/layout';
-import { Breadcrumb } from 'office-ui-fabric-react/lib/Breadcrumb';
-import { fetchUsers } from '../../actions';
+import {
+  CommandBar,
+  Breadcrumb,
+  Spinner,
+  SpinnerSize,
+  DetailsList,
+  DetailsListLayoutMode
+} from 'office-ui-fabric-react';
+import { fetchBuildings } from '../../actions';
+import _columns from './columns';
 const { Content } = Layout;
-// Redux
 
-class UsersList extends React.Component {
-  componentWillMount() {
-    this.props.fetchUsers();
+const items = [
+  {
+    key: 'addRow',
+    name: 'Agregar evento',
+    icon: 'Add'
+  },
+  {
+    key: 'deleteRow',
+    name: 'Editar evento',
+    icon: 'Edit'
+  }
+];
+
+class BuildingsList extends React.Component {
+  componentWillMount () {
+    this.props.fetchBuildings();
   }
 
-  render() {
+  onRenderColumn = (item, index, column) => {
+    const fieldContent = item[column.fieldName];
+    if (column.key.includes('date')) {
+      return <div>{moment(fieldContent).format('DD/MM/YYYY')}</div>;
+    }
+    if (column.key === 'map') {
+      console.log(fieldContent);
+      const [lat, long] = fieldContent.split(',');
+      console.log(lat);
+      console.log(long);
+      const url = `https://www.google.com/maps/preview/@${lat},${long},15z`;
+      return <a href={url} target="_blank">MAPA</a>;
+    }
+    console.log(fieldContent);
+    return fieldContent;
+  }
+
+  render () {
     if (this.props.loginData.loggedIn === false) {
+      // TODO redirect to /
       return null;
     }
-    if (!Array.isArray(this.props.users)) {
-      return <div>Loading ...</div>;
+    if (!Array.isArray(this.props.buildings) || this.props.buildings.length === 0) {
+      return <Spinner size={SpinnerSize.large} label="Cargando..." />;
     }
     return (
       <Content style={{ padding: '0 50px' }}>
         <Breadcrumb
           items={[
-          { text: 'Home', key: 'home', href: '/' },
-          { text: 'Users', key: 'users' }
+            { text: 'Inicio', key: 'home', href: '/' },
+            { text: 'Edificios', key: 'buildings', href: '#' }
           ]}
         />
-        <h1>Users</h1>
-        {this.props.users.map(u => <div key={u._id}>{`*${u.firstName} ${u.lastName}`}</div>)}
+        <div
+          className="ms-Grid"
+          style={{ backgroundColor: 'white', minHeight: 200, padding: 5, marginTop: 10 }}
+        >
+         <CommandBar items={items} />
+        <DetailsList
+              items={this.props.buildings}
+              columns={_columns}
+              onRenderItemColumn={this.onRenderColumn}
+              setKey="set"
+              layoutMode={DetailsListLayoutMode.fixedColumns}
+              selectionPreservedOnEmptyClick
+            />
+        </div>
       </Content>
     );
   }
 }
 
-function mapStateToProps({ users, loginData }) {
-  console.log(users);
+function mapStateToProps ({ buildings, loginData }) {
   return {
     loginData,
-    users: users.users
+    buildings: buildings.buildings
   };
 }
 
-export default connect(mapStateToProps, { fetchUsers })(UsersList);
-  
+export default connect(mapStateToProps, { fetchBuildings })(BuildingsList);
--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,14 +1,16 @@
 import { combineReducers } from 'redux';
 
-import users from './reducer_users.js'
-import events from './reducer_events.js'
+import users from './reducer_users.js';
+import events from './reducer_events.js';
+import buildings from './reducer_building';
 import loginData from './reducer_login';
 
 
 const rootReducer = combineReducers({
-	users,
-	events,
-	loginData
+  users,
+  events,
+  buildings,
+  loginData
 });
 
 export default rootReducer;
